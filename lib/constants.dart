dbd750911da0c305958977747feab380226d6aae
Aarón Montes <aaron@kssco.com>
2021-06-16 18:48:50 -0600

    Adding function to determine if a color it's dark or light

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+final double rConstant = 0.299;
+final double gConstant = 0.587;
+final double bConstant = 0.114;
+final double hspBoundary = 127.5;
--- lib/flutter_is_dark_color_hsp.dart
+++ lib/flutter_is_dark_color_hsp.dart
@@ -1,7 +1,22 @@
 library flutter_is_dark_color_hsp;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_is_dark_color_hsp/constants.dart';
+
+/// Function to determine if a color is dark tone. It's based on HSP Color Model
+/// color: Argument to determine its status
+bool isDarkHsp(Color color) {
+  int _redValue = color.red;
+  int _greenValue = color.green;
+  int _blueValue = color.blue;
+  double _hsp = sqrt(rConstant * pow(_redValue, 2) + gConstant * pow(_greenValue, 2) + bConstant * pow(_blueValue, 2));
+  return _hsp < hspBoundary;
+}
+
+/// Function to determine if a color is light tone. It's based on HSP Color Model
+/// color: Argument to determine its status
+bool isLightHsp(Color color) {
+  return !isDarkHsp(color);
 }
